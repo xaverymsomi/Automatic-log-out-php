e166ec46f0e8614317d9927e27998a6b839a29e3
Vicent Msomi <msomivicent@gmail.com>
2025-05-16 14:18:27 +0300

    remove risk

--- conn.php
+++ conn.php
@@ -3,9 +3,6 @@ $strServer="localhost"; // Server IP Address Or Name
 $strUser="root"; // User ID
 $strPwd="";// Password
 
-//$strUser="zeabros_zipl"; // User ID
-//$strPwd="E5pOMvTR;[=&";// Password
-
 $con=mysqli_connect($strServer,$strUser,$strPwd,"test_table");
 
 ?>
